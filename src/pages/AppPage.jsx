df8e93aa856926ba990845ecc0128d5dda90f5f6
Makhiedel <muwwwwwwwww@gmail.com>
2025-09-24 00:46:38 +0300

    fix1

--- src/pages/AppPage.jsx
+++ src/pages/AppPage.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css"; // Import default styles
-import "./AppPageStyle.css";
+import "./AppPageStyle.css"; // Fix the path to the correct file
 
 const CalendarComponent = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
